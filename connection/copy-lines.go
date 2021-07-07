88b22d286125c6028b74cd10154508c7766950db
Andrea Parodi <andrea@parro.it>
2021-07-07 11:54:35 +0200

    log from files in a remote ssh server

--- connection/copy-lines.go
+++ connection/copy-lines.go
@@ -9,32 +9,62 @@ import (
 
 	"github.com/meteocima/virtual-server/tailor"
 	"github.com/meteocima/virtual-server/vpath"
+	"golang.org/x/crypto/ssh"
 )
 
 func copyLines(proc Process, w io.Writer, outLogFile vpath.VirtualPath) {
-	var logFile *os.File
-	var err error = errors.New("empty")
-	for err != nil {
-		logFile, err = os.Open(outLogFile.Path)
-		if os.IsNotExist(err) {
-			time.Sleep(100 * time.Millisecond)
-			continue
+	if outLogFile.Host == "localhost" {
+		var logFile *os.File
+		var err error = errors.New("empty")
+		for err != nil {
+			logFile, err = os.Open(outLogFile.Path)
+			if os.IsNotExist(err) {
+				time.Sleep(100 * time.Millisecond)
+				continue
+			}
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "WARNING: copyLines error: (os.Open `%s`\n): %s", outLogFile.Path, err.Error())
+				return
+			}
 		}
 
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "WARNING: copyLines error: (os.Open `%s`\n): %s", outLogFile.Path, err.Error())
-			return
-		}
+		go func() {
+			tailProc := tailor.New(logFile, w, 1024)
+			errs := tailProc.Start()
+			proc.Wait()
+			tailProc.Stop()
+			err := <-errs
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "WARNING: copyLines error (reading lines from `%s`): %s\n", outLogFile.Path, err.Error())
+			}
+		}()
+
+		return
+	}
+
+	var conn *ssh.Client
+	cmd, err := conn.NewSession()
+	if err != nil {
+		panic(fmt.Errorf("copyLines to log from %s: conn.client.NewSession: %w", outLogFile.String(), err))
+	}
+	defer cmd.Close()
+
+	cmdStr := fmt.Sprintf("tail -F '%s'", outLogFile.Path)
+
+	err = cmd.Start(cmdStr)
+	if err != nil {
+		panic(fmt.Errorf("copyLines to log from %s: cmd.Start: %w", outLogFile.String(), err))
 	}
 
 	go func() {
-		tailProc := tailor.New(logFile, w, 1024)
-		errs := tailProc.Start()
-		proc.Wait()
-		tailProc.Stop()
-		err := <-errs
+		_, err := proc.Wait()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "WARNING: copyLines error (reading lines from `%s`): %s\n", outLogFile.Path, err.Error())
+			panic(fmt.Errorf("copyLines to log from %s: proc.Wait: %w", outLogFile.String(), err))
+		}
+		err = cmd.Signal(ssh.SIGKILL)
+		if err != nil {
+			panic(fmt.Errorf("copyLines to log from %s: cmd.Signal(ssh.SIGKILL): %w", outLogFile.String(), err))
 		}
 	}()
 
